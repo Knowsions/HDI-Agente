abef02fa96d6776890ff20b79864d1940a9756ae
karim <wody_elyp@hotmail.com>
2016-04-23 00:48:51 -0500

    Initial Commit

---
+++ HDI Agente/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HDI Agente
+//
+//  Created by karim herrera on 23/04/16.
+//  Copyright © 2016 In-die Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
